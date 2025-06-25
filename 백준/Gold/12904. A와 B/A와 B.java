92c6386241d00ada9d667d8eb325ec9433279de0
김민서 <ms9648@naver.com>
2025-06-25 20:11:31 +0900

    [Gold V] Title: A와 B, Time: 80 ms, Memory: 14868 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String S = br.readLine();
+        String T = br.readLine();
+        
+        // T -> S
+        while(true) {
+            if(T.equals(S)) {
+                System.out.println(1);
+                break;
+            }
+            
+            if(T.length() == 0) {
+                System.out.println(0);
+                break;
+            }
+            
+            if(T.charAt(T.length() - 1) == 'B') {
+                T = T.substring(0, T.length() - 1);
+                T = new StringBuilder(T).reverse().toString();
+            } else {
+                T = T.substring(0, T.length() - 1);
+            }
+        }
+    }
+}
